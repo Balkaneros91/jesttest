de43aec6eadc0e5f542bba143eb22e27d101a2a8
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-02-08 12:11:08 +0000

    Add calculator and addition!

---
+++ scripts/calc.js
@@ -0,0 +1,10 @@
+function addition (x, y) {      // my code
+    let z = x + y;
+    return z;
+}
+
+// function addition (num1, num2) {     // video code 
+//     return num1 + num2;
+// }
+
+module.exports = addition;
